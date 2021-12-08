b9a080b6c67d3efdcded5f076f7fe8744b9d00ba
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-08 08:30:09 -0500

    Add animate.css library & bounce anim to tree

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const playBtn = document.getElementById('play-btn')
 const pauseBtn = document.getElementById('pause-btn')
 const stopBtn = document.getElementById('stop-btn')
 const bellIcon = document.getElementById('bell')
+const animations = document.querySelectorAll('.animation')
 const audio = new Audio('bells.mp3')
 audio.loop = true
 
@@ -11,18 +12,18 @@ stopBtn.addEventListener('click', stop)
 
 function play() {
   audio.play()
-  bellIcon.style.animationPlayState = 'running'
+  animations.forEach((item) => (item.style.animationPlayState = 'running'))
 }
 
 function pause() {
   audio.pause()
-  bellIcon.style.animationPlayState = 'paused'
+  animations.forEach((item) => (item.style.animationPlayState = 'paused'))
 }
 
 function stop() {
   audio.pause()
   audio.currentTime = 0
-  bellIcon.style.animationPlayState = 'paused'
+  animations.forEach((item) => (item.style.animationPlayState = 'paused'))
 }
 
 // Task:
